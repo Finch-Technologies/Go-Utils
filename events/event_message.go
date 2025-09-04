b1616cd54edf54f5ca14c91aebbdcc1cef581841
ryanlake <ryan.l@finch-technologies.com>
2025-09-04 14:31:38 +0200

    Removed event message from utils

--- events/event_message.go
+++
@@ -1,9 +0,0 @@
-package events
-
-type EventMessage struct {
-	SessionId      string `json:"sessionId"`
-	Event          Event  `json:"event"`
-	DisplayTitle   string `json:"displayTitle"`
-	DisplayMessage string `json:"displayMessage"`
-	Data           any    `json:"data"`
-}
